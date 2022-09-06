feb87439da2bd6451fb3249d5a3b8644117c05cf
Sa Hyun Hwang <4hyun8@gmail.com>
2022-09-06 19:28:30 +0900

    successfully scrape pagination when there is one

--- src/scraper/scraper.service.ts
+++ src/scraper/scraper.service.ts
@@ -4,13 +4,17 @@ import { Browser } from 'puppeteer';
 
 import { NAVER_BLOG_URL } from '../naver-blog-scraper/lib/naver-blog-scraper';
 import { TOCTitleFactory } from './classes/TOCTitle';
-import * as fs from 'fs';
-import * as path from 'path';
 import { ROOT_TOC_TITLES } from './constants/page-object';
 import { StorageService } from 'src/storage/storage.service';
 import * as cheerio from 'cheerio';
 
 const MAIN_IFRAME_SELECTOR = 'iframe#mainFrame';
+const POSTS_FROM_CHILD_TITLE_PAGE_SELECTOR =
+  'div#postListBody > div.post._post_wrap > div.post-back';
+const PAGINATION_WRAPPER_SELECTOR = 'div.wrap_blog2_paginate';
+const PAGINATION_ITEMS_SELECTOR = 'div.blog2_paginate > a.page';
+const NEXT_PAGE_ITEM_SELECTOR = 'div.blog2_paginate > strong.page + a';
+const PAGINATION_ITEMS_NEXT_SET = 'div.blog2_paginate > a.next';
 const TOC_DATA_PATH = '/scraped/toc.json';
 const SKIP_TOC = ['23'];
 
@@ -22,16 +26,22 @@ export class ScraperService {
     private storageService: StorageService,
   ) {}
 
+  async writePostDataToFile(posts: any[]) {
+    posts.map(async (data) => {
+      const disk = this.storageService.getDisk('local');
+      await disk.put(data.file, JSON.stringify(data));
+    });
+  }
+
   async scrapeBlog() {
     const createTocTitle = this.tocTitle.create;
     const version = await this.browser.version();
     const page = await this.browser.newPage();
+    const postPage = await this.browser.newPage();
     await page.goto(NAVER_BLOG_URL);
     await page.screenshot({ path: 'screenshot.png' });
     const mainIframeElement = await page.$(MAIN_IFRAME_SELECTOR);
     const mainIframe = await mainIframeElement.contentFrame();
-
-    // const iframeContent = await mainIframe.content();
 
     page.on('console', (msg) => console.log('msg.args: ', msg.text()));
 
@@ -62,58 +72,68 @@ export class ScraperService {
       });
     });
 
-    const rootTOCTitles = rootTitles.map((title) => {
+    const tableOfContentsRootTitles = rootTitles.map((title) => {
       const tocTitle = this.tocTitle.create(title);
       return tocTitle;
     });
 
     /** This method sets `children` property on the Root TOC Titles scraped prior */
-    const getChildrenTitles = async () => {
-      return await Promise.all(
-        rootTOCTitles.map(async (tocTitle) => {
-          const childrenTitles = await mainIframe.$$eval(
-            tocTitle.getChildrenTitleCssSelector(),
-            ($childrenTitles) => {
-              return $childrenTitles.map(($childTitle) => {
-                const [$anchor, $postCount] = $childTitle.children;
-
-                const categoryId = $anchor.id.replace('category', '');
-
-                const href = $anchor.getAttribute('href').trim();
-
-                const postCount = `${
-                  $postCount == null ? '(0)' : $postCount.textContent
-                }`
-                  .replace('(', '')
-                  .replace(')', '');
-                const titleText = $anchor.textContent.trim();
-
-                // TOC title
-                const childTitle = {
-                  title: titleText,
-                  href,
-                  total: postCount,
-                  categoryId,
-                };
-
-                return childTitle;
-              });
-            },
-          );
-          if (childrenTitles.length > 0)
-            tocTitle.setChildren(childrenTitles.map(createTocTitle));
-
-          return tocTitle;
-        }),
-      );
+    const loopTableOfContentsRootTitles = (
+      fn: (...args: any[]) => Promise<any>,
+    ) => {
+      return tableOfContentsRootTitles.map(fn);
     };
 
-    await getChildrenTitles();
-    const rootTOCTitlesFiltered = rootTOCTitles.filter(
+    const mapChildrenValueOfTableOfContentsRootTitle = async (tocTitle) => {
+      const childrenTitles = await mainIframe.$$eval(
+        tocTitle.getChildrenTitleCssSelector(),
+        ($childrenTitles) => {
+          return $childrenTitles.map(($childTitle) => {
+            const [$anchor, $postCount] = $childTitle.children;
+
+            const categoryId = $anchor.id.replace('category', '');
+
+            const href = $anchor.getAttribute('href').trim();
+
+            const postCount = `${
+              $postCount == null ? '(0)' : $postCount.textContent
+            }`
+              .replace('(', '')
+              .replace(')', '');
+            const titleText = $anchor.textContent.trim();
+
+            // TOC title
+            const childTitle = {
+              title: titleText,
+              href,
+              total: postCount,
+              categoryId,
+            };
+
+            return childTitle;
+          });
+        },
+      );
+      if (childrenTitles.length > 0)
+        tocTitle.setChildren(childrenTitles.map(createTocTitle));
+
+      return tocTitle;
+    };
+
+    const getChildrenOfRootTitles = async () => {
+      await Promise.all(
+        loopTableOfContentsRootTitles(
+          mapChildrenValueOfTableOfContentsRootTitle,
+        ),
+      );
+    };
+    await getChildrenOfRootTitles();
+
+    const filteredRootTitles = tableOfContentsRootTitles.filter(
       ({ categoryId }) => !SKIP_TOC.includes(categoryId),
     );
     const disk = this.storageService.getDisk('local');
-    await disk.put(TOC_DATA_PATH, JSON.stringify(rootTOCTitlesFiltered));
+    await disk.put(TOC_DATA_PATH, JSON.stringify(filteredRootTitles));
 
     // const samplePost = posts[1];
     const topLevelTitles = await this.getTitlesFromFile();
@@ -129,84 +149,186 @@ export class ScraperService {
         const urlObj = new URL(url);
         const { protocol, host } = urlObj;
         baseUrl = `${protocol}//${host}`;
-        console.log('SET_BASEURL : ', baseUrl);
       };
-      // const testPage = await this.browser.newPage();
-      // const mainIframe = testPage;
+
+      //* Loop Root Titles
       for (const title of topLevelTitles) {
+        let breakLoop = false;
+        const setBreakLoop = (v: boolean) => (breakLoop = v);
+        // console.log('DEBUG title : ', title);
         if (isBaseUrlEmpty()) setBaseUrl(mainIframe.url());
 
-        for (const child of title.children) {
-          const titleLink = baseUrl + child.href;
-          await mainIframe.goto(titleLink);
-          console.log('DEBUG testPage url : ', mainIframe.url(), '\n');
-          /*         await mainIframe.page().screenshot({ path: 'sample-post-page.png' }); */
+        //* Loop Child Titles
+        if (Array.isArray(title.children))
+          for (const child of title.children) {
+            const titleLink = baseUrl + child.href;
+            await mainIframe.goto(titleLink);
+            // console.log('DEBUG testPage url : ', mainIframe.url(), '\n');
 
-          const getPostDataOnInnerPage = async (pageIndex = '1') => {
-            const mainIframeElement = await page.$(MAIN_IFRAME_SELECTOR);
-            const mainIframe = await mainIframeElement.contentFrame();
-            const postsHtmlList = await mainIframe.$$eval(
-              'div#postListBody > div.post._post_wrap > div.post-back',
-              ($posts) => {
-                console.log('$posts count : ', $posts.length);
-                return $posts.map(($post, i) => {
-                  return {
-                    html: $post.innerHTML,
-                  };
-                });
-              },
-            );
+            const getPostDataOnCurrentPage = async (pageIndex = '1') => {
+              const mainIframeElement = await page.$(MAIN_IFRAME_SELECTOR);
+              const mainIframe = await mainIframeElement.contentFrame();
+              const postsHtmlList = await mainIframe.$$eval(
+                POSTS_FROM_CHILD_TITLE_PAGE_SELECTOR,
+                ($posts) => {
+                  return $posts.map(($post, i) => {
+                    return {
+                      html: $post.innerHTML,
+                    };
+                  });
+                },
+              );
 
-            const postData = postsHtmlList.map(({ html }) => {
-              const getPostId = () => {
+              const postData = postsHtmlList.map((thePostHtml) => {
+                const { html } = thePostHtml;
+                const getPostId = () => {
+                  const $ = cheerio.load(html);
+
+                  let postUrl = $('input.copyTargetUrl').val();
+                  if (Array.isArray(postUrl)) postUrl = postUrl[0];
+                  return postIdFromUrl(postUrl);
+                };
+                const postId = getPostId();
+                const file = `/scraped/post-${postId}.json`;
                 const $ = cheerio.load(html);
+                const title = $('.htitle > span.itemSubjectBoldfont').text();
+                const cHeadingTitle = $(
+                  '#postViewArea .c-heading__title',
+                ).text();
+                const cHeadingContent = $(
+                  '#postViewArea .c-heading__content',
+                ).text();
+                // debugGetPostContent({ title, cHeadingTitle, cHeadingContent, postId });
+                return {
+                  postId,
+                  file,
+                  title,
+                  cHeadingTitle,
+                  cHeadingContent,
+                  categoryId: child.categoryId,
+                  //! Do not include html for now as there is no use for it yet and it is large
+                  // html,
+                };
+              });
 
-                let postUrl = $('input.copyTargetUrl').val();
-                if (Array.isArray(postUrl)) postUrl = postUrl[0];
-                return postIdFromUrl(postUrl);
-              };
-              const postId = getPostId();
-              const file = `/scraped/post-${postId}.json`;
-              const $ = cheerio.load(html);
-              const title = $('.htitle > span.itemSubjectBoldfont').text();
-              const cHeadingTitle = $('#postViewArea .c-heading__title').text();
-              const cHeadingContent = $(
-                '#postViewArea .c-heading__content',
-              ).text();
-              // debugGetPostContent({ title, cHeadingTitle, cHeadingContent, postId });
-              return {
-                postId,
-                file,
-                title,
-                cHeadingTitle,
-                cHeadingContent,
-                html,
-              };
-            });
+              return postData;
+            };
 
-            return postData;
-          };
+            //* Get and Save First Page Post Data
+            const postData = await getPostDataOnCurrentPage();
+            await this.writePostDataToFile(postData);
 
-          const postData = await getPostDataOnInnerPage();
-          console.log(
-            `postData on innerPage of titlePage "${child.title}"`,
-            postData.length,
-          );
+            //* Loop Pagination (if exists)
+            /*  const getPaginationItemOnCurrentPage = async () => {
+              const mainIframe = await mainIframeElement.contentFrame();
+
+              const result = await mainIframe.$$eval(
+                PAGINATION_ITEMS_SELECTOR,
+                ($paginationItems) => {
+                  console.log(
+                    '$paginationItems count: ',
+                    $paginationItems.length,
+                  );
+                  return $paginationItems;
+                },
+              );
+              return { result };
+            };
+
+            const { result: currentPagePaginationItems } =
+              await getPaginationItemOnCurrentPage();
+
+            if (currentPagePaginationItems.length > 0) {
+              for (const thePage of currentPagePaginationItems) {
+                const postData = await getPostDataOnCurrentPage();
+                console.log(
+                  `postData on innerPage of titlePage "${child.title}"`,
+                  postData.length,
+                );
+                await this.writePostDataToFile(postData);
+              }
+            } */
+
+            //* check for next-pagination-item
+            /* 
+            const navigatePostPage = async (path: string) => {
+              await postPage.goto('https://blog.naver.com' + path);
+            };
+            const getPostPageIframe = async () => {
+              return await postPage.$(MAIN_IFRAME_SELECTOR);
+            };
+            const initializePostPage = async (startingPostPagePath: string) => {
+              await navigatePostPage(startingPostPagePath);
+            }; */
+            const getNextPageItem = async () => {
+              const result = await mainIframe.$$eval(
+                NEXT_PAGE_ITEM_SELECTOR,
+                ($nextPageItem) => {
+                  console.log(
+                    '[1] getNextPageItem() $nextPageItem.length: ',
+                    $nextPageItem.length,
+                  );
+                  let result = null;
+                  if ($nextPageItem.length === 1) {
+                    const href = $nextPageItem[0].getAttribute('href');
+                    result = { href };
+                  }
+                  return result;
+                },
+              );
+
+              console.log('[2] getNextPageItem() result: ', result, '\n');
+              return result;
+            };
+
+            const navigateToNextPage = async (path: string) => {
+              const mainIframe = await mainIframeElement.contentFrame();
+              await mainIframe.goto('https://blog.naver.com' + path);
+            };
+
+            const getCurrentFrameUrl = () => {
+              return mainIframe.url();
+            };
+
+            // const nextPageItem = await getNextPageItem();
+            // console.log(nextPageItem);
+            let nextPageItem = await getNextPageItem();
+            let stopped = nextPageItem == null;
+            while (!stopped) {
+              console.log(`Paginated.`);
+              await navigateToNextPage(nextPageItem.href);
+              const debugUrl = new URL(getCurrentFrameUrl());
+              // page.screenshot({ path: `${debugUrl.toString()}.png` });
+              const currentPage = debugUrl.searchParams.get('currentPage');
+              // console.log(`pagination for url : ${getCurrentFrameUrl()}`);
+              console.log(`paginating for title: ${child.title}
+pagination for page:${currentPage}
+currentFrame url: ${debugUrl.toString()}
+`);
+
+              const postData = await getPostDataOnCurrentPage();
+              console.log('>> postData: ', postData);
+              await this.writePostDataToFile(postData);
+              nextPageItem = await getNextPageItem();
+              stopped = nextPageItem == null;
+            }
+            // return;
+
+            //! 'break' during development to NOT loop everything
+            if (child.categoryId === '40') {
+              /* 가족갈등.. */ setBreakLoop(true);
+              break;
+            }
+          }
+        if (breakLoop) {
+          break;
         }
-
-        return;
-        /*         const writePostDataToFile = async (posts: any[]) => {
-          posts.map(async (data) => {
-            await disk.put(data.file, JSON.stringify(data));
-          });
-        };
-        await writePostDataToFile(postData); */
       }
     };
 
     await scrapePostsAndSave();
 
-    return { result: { toc: rootTOCTitles } };
+    return { result: { toc: tableOfContentsRootTitles } };
   }
 
   async getTitlesFromFile(): Promise<any[]> {
@@ -232,6 +354,6 @@ function debugGetPostContent({
 }
 
 function postIdFromUrl(url: string) {
-  console.log('postIdFromUrl() url argument: ', url);
+  // console.log('postIdFromUrl() url argument: ', url);
   return url.split('/').pop();
 }
